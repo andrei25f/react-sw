c8e5f28544de96d443797cedc22b0a250aa7ba9c
Andrey <andrey25f@gmail.com>
2024-09-18 10:24:12 +0200

    Generic withErrorPage

--- src/hoc/withErrorPage.tsx
+++ src/hoc/withErrorPage.tsx
@@ -1,10 +1,11 @@
-import { FC, useContext, useEffect } from "react";
+import { Component, ComponentType, FC, useContext, useEffect } from "react";
 import { characters, defaultHero } from "../utils/constants";
 import { SWContext } from "../utils/context";
 import { useParams } from "react-router-dom";
 import ErrorPage from "../components/ErrorPage";
 
-export const withErrorPage = (Component: FC<any>) => (props: any) => {
+//export const withErrorPage = (Component: FC<any>) => (props: any) => {
+export const withErrorPage = <T extends object>(Component: ComponentType<T>) => (props: T) => {
     const { heroId = defaultHero } = useParams();
     const { changeHero, setIsError } = useContext(SWContext);
 
